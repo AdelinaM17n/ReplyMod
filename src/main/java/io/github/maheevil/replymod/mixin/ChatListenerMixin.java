d0d9eff31945ef75e5a8d42fd7d3658685ee4985
Maheesha <maheeshasithum@gmail.com>
2023-06-26 10:06:09 +0530

    Completely redone the mixin

---
+++ src/main/java/io/github/maheevil/replymod/mixin/ChatListenerMixin.java
@@ -0,0 +1,37 @@
+package io.github.maheevil.replymod.mixin;
+
+import com.mojang.authlib.GameProfile;
+import io.github.maheevil.replymod.ReplyMod;
+import net.minecraft.client.multiplayer.chat.ChatListener;
+import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.ChatTypeDecoration;
+import net.minecraft.network.chat.PlayerChatMessage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ChatListener.class)
+public class ChatListenerMixin {
+    @Inject(
+            method = "handlePlayerChatMessage",
+            at = @At(
+                    value = "HEAD"
+            )
+    )
+    private void replymod$handlePlayerChatMessage(
+            PlayerChatMessage chatMessage, GameProfile gameProfile, ChatType.Bound boundChatType, CallbackInfo ci
+    ){
+        ChatTypeDecoration chat = boundChatType.chatType().chat();
+        String name = chat.parameters().get(0).getSerializedName();
+
+        if(!chat.style().isEmpty()){
+            if(name.equalsIgnoreCase("sender")){
+                ReplyMod.lastMessenger = gameProfile.getName();
+            } else {
+                assert boundChatType.targetName() != null;
+                ReplyMod.lastMessenger = boundChatType.targetName().getString();
+            }
+        }
+    }
+}
